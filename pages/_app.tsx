623ec7eb92b24b80b9431ba74b9c11262748589d
ilyesnajjari <ilyes.najjari@telecom-st-etienne.fr>
2025-05-25 12:57:20 -0400

    version X.4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,12 +10,14 @@ import "../styles/fiche-detail.css";
 
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
