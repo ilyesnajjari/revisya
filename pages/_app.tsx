ac9f7026e9531849861e1f3b7149c530a066844a
ilyesnajjari <ilyes.najjari@telecom-st-etienne.fr>
2025-05-25 12:52:50 -0400

    version X.3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 // filepath: /pages/_app.tsx
+
 import "../styles/Footer.css";
 import "../styles/Header.css";
 import "../styles/SearchFiches.css";
 import "../styles/index.css";
 import "../styles/contact.css";
 import "../styles/cours.css";
-import '../styles/fiche-detail.css';
-import "../styles/index.css";
+import "../styles/fiche-detail.css";
 
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
