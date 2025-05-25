99f167fccd66f1ef74c85f9949c9419f00a9e3b6
ilyesnajjari <ilyes.najjari@telecom-st-etienne.fr>
2025-05-25 14:16:25 -0400

    Version X.13

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+// pages/_document.tsx
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="fr">
-        <Head />
+      <Html lang="fr"> {/* <-- corrige aussi l'erreur de "lang" manquant */}
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="5nANXr2L6_eGycf6iHvDMvrqu02gtCbo18CcE-5-pDw"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
